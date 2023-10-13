ae538e6ea9d0f9e62f8066854f75e6424d2dfc8f
Eric Jackman <jackmane1@mymail.nku.edu>
2023-10-13 03:27:29 -0400

    Added signup and login pages

---
+++ apps/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../themes/css/loginAndSignup.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Home page goes here</h1>
+    </div>
+</body>
+</html>
